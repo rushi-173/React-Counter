41d4ccf278cf929c08869c75e0684170c714a615
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-02-14 15:52:53 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter</h1>
+      <h1>0</h1>
+      <button>+</button>
     </div>
   );
 }
