0f91aea070d34755f8a021b795e61403ff9d1168
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-02-14 16:01:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [value, setValue] = useState(0);
+  function Add() {
+    setValue(value + 1);
+  }
+  function Subtract() {
+    setValue(value - 1);
+  }
   return (
     <div className="App">
       <h1>Counter</h1>
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{value}</h1>
+      <button onClick={Add}>+</button>
+      <button onClick={Subtract}>-</button>
     </div>
   );
 }
